bed0ac4f4ab0c813fcab4501ac15445b0285bb60
David Maul <dmaul12@gmail.com>
2016-08-14 15:52:11 -0400

    changes to background

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -79,14 +79,17 @@ function showSaveBrews(){
       console.log(brewlist)
       brewlist.forEach(function(brew){
         console.log(brew.id)
-        // var $a      = $('a').attr('href', brew.website)
+          var $name       = $('<a target="_blank" href="' + brew.website + '">')
+      $name.text( brew.name );
+        var $a      = $('a').attr('href', brew.website)
         var $img       = $('<img>').attr('src', brew.img)
         var $delete     =$('<button class="delete">').text("Delete Brewery")
         var $oneresult=$('<div class = "one">')
         var $div= $('<div>').text(brew.name +' '+brew.website).val(brew.id)
+        // var $div= $('<div>').attr($name).val(brew.id)
         console.log(brew.name, brew.website)
         // console.log($a)
-        $oneresult.append($div).append($delete).append($img)
+        $oneresult.append($div).append($delete)
         $container.append($oneresult)
 
         $delete.click(deleteBrews)
